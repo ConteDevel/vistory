b77c0aca4cfc2482f0da05f8c9c3cd73e970f6cc
Denis Sologub <contedevel2010@gmail.com>
2018-10-01 07:15:25 +0300

    Front: Adds a sidebar listener on item selected event

--- frontend/website/static/js/app.js
+++ frontend/website/static/js/app.js
@@ -1,25 +1,44 @@
-function getHost() {
-    let arr = window.location.href.split("/");
-    return arr[0] + "//" + arr[2]
-}
-
-Vue.component('main-menu', {
-    beforeMount() {
-        window.hasAccessToken = initOAuth2();
-    },
-    data: function () {
-        return {
-            signed: window.hasAccessToken
+let rightMenuItem = {
+    delimiters: ['{(', ')}'],
+    template: '#right-menu-item',
+    props: ['title', 'icon', 'active', 'index', 'listener'],
+    methods: {
+        toggleRowActive() {
+            this.$emit('newactive', this.index);
+            this.listener(this.index);
         }
-    },
-});
+    }
+};
 
 let app = new Vue({
+    delimiters: ['{(', ')}'],
     beforeMount() {
         window.hasAccessToken = initOAuth2();
+        this.signed = window.hasAccessToken;
+        this.mainTitle = this.sections[this.selectedIndex].title;
     },
-    el: '#app',
+    el: '#page',
+    components: {
+        'right-menu-item': rightMenuItem
+    },
     data: {
-        message: window.hasAccessToken ? 'Hello, User!' : 'Hello, Anonymous!'
+        signed: window.hasAccessToken,
+        sections: [
+            {id: 'profile', title: 'Profile', icon: 'fa-user'},
+            {id: 'posts', title: 'Posts', icon: 'fa-images'},
+            {id: 'people', title: 'People', icon: 'fa-users'},
+            {id: 'channels', title: 'Channels', icon: 'fa-stream'}
+        ],
+        selectedIndex: 1,
+        mainTitle: null
+    },
+    methods: {
+        /**
+        * On right menu item selected listener
+        */
+        onItemSelected(index) {
+            this.selectedIndex = index;
+            this.mainTitle = this.sections[index].title
+        }
     }
 });
