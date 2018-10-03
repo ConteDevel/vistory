34290f68018887cf0748e3a42eb4db08dd866eb4
Denis Sologub <contedevel2010@gmail.com>
2018-10-03 17:23:56 +0300

    Front: adds the user profile information

--- frontend/website/templates/pages/profile.vue
+++ frontend/website/templates/pages/profile.vue
@@ -1,3 +1,37 @@
 <template>
-    <h2 class="subtitle">Profile</h2>
+    <div class="card">
+        <div class="card-content">
+            <div class="media">
+                <div class="media-left">
+                    <figure class="image is-48x48">
+                        <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                    </figure>
+                </div>
+                <div class="media-content">
+                    <p class="title is-4">{( me.first_name + ' ' + me.last_name )}</p>
+                    <p class="subtitle is-6">{( me.email )}</p>
+                    <p class="subtitle is-6">
+                        <strong>Role: </strong>{( me.role )}<br/>
+                        <strong>Gender: </strong>{( me.gender )}<br/>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+<script>
+    module.exports = {
+        beforeMount() {
+            getMe().then(response => {
+                this.me = JSON.parse(response.data);
+                console.log(this.me);
+            });
+        },
+        delimiters: ['{(', ')}'],
+        data: function () {
+            return {
+                me: {}
+            }
+        }
+    }
+</script>
